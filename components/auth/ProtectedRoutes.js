41980b6cee9b78db3c5747d44dbad785f5510aeb
Ayush Gaikwad <ayushgaikwad0605@gmail.com>
2025-11-28 14:55:21 +0530

    middleware fixed

--- components/auth/ProtectedRoutes.js
+++ components/auth/ProtectedRoutes.js
@@ -1,18 +1,25 @@
 "use client";
-import { useEffect } from "react";
+
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 
-export default function ProtectedRoutes ({ children }) {
+export default function ProtectedRoutes({ children }) {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const token = Cookies.get("accessToken");
-
     if (!token) {
       router.replace("/login");
+    } else {
+      setIsLoading(false);
     }
-  }, []);
+  }, [router]);
+
+  if (isLoading) {
+    return <div>Loading...</div>; // or a spinner
+  }
 
   return <>{children}</>;
 }
