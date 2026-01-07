2aa9e672273ab2be7f9c6af2e2dd65bca2159d03
Ayush Gaikwad <ayushgaikwad0605@gmail.com>
2026-01-07 17:57:07 +0530

    middleware updated

--- middleware.js
+++ middleware.js
@@ -1,24 +1,9 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  const pathname = req.nextUrl.pathname;
-
-  // Read token from cookies
-  const token = req.cookies.get("accessToken")?.value;
-
-  // If user opens "/" and has no token --> redirect to login
-  if (pathname === "/" && !token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  return NextResponse.next();
-}
-
-export function middleware(req) {
   console.log("MIDDLEWARE COOKIES 👉", req.cookies.getAll());
   return NextResponse.next();
 }
-
 
 export const config = {
   matcher: [
